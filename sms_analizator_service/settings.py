441c8d1638c8f66fa5c34ec6cffc2a87db112734
Xopek <scream6222@gmail.com>
2024-11-21 14:18:06 +0300

    debug

--- sms_analizator_service/settings.py
+++ sms_analizator_service/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/templates/'
-STATIC_ROOT = [os.path.join(BASE_DIR, 'templates')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates')]
+STATIC_ROOT  = [os.path.join(BASE_DIR, 'templates')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
