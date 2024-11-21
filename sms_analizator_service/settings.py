82609741b6fe772e85f5c373d7e0df6a848dc585
Xopek <scream6222@gmail.com>
2024-11-21 14:39:30 +0300

    debug

--- sms_analizator_service/settings.py
+++ sms_analizator_service/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d5fj(n7()5we1+6@mcvg53uu0mbg8_=d5k0qbbf+sif-8klua&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
